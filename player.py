9ec2cdfa6dc927804f49271691871d4c6a7610e5
jandirdlctm <d00332071@dmail.dixie.edu>
2021-09-18 22:13:53 -0600

    green

--- player.py
+++ player.py
@@ -1,5 +1,7 @@
 class SoccerPlayer:
     def __init__(self,pace, shooting, passing, dribbling, defending, physical):
+        if pace < 0 or shooting < 0 or passing < 0 or dribbling < 0 or defending < 0 or physical < 0:
+            raise ValueError
         self.mPace = pace
         self.mShooting = shooting
         self.mPassing = passing
