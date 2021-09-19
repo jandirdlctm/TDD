2160f2fd9c82c7ce2eac07b6f2aa9bdbb4a39ac9
jandirdlctm <d00332071@dmail.dixie.edu>
2021-09-18 20:45:55 -0600

    refactor

--- test.py
+++ test.py
@@ -2,7 +2,15 @@ import pytest
 from player import SoccerPlayer
 
 def describe_SoccerPlayer():
+    
     def describe_initializer_of_soccer_player():
         def it_assigns_all_six_attributes():
                 newPlayer = SoccerPlayer(23,67,45,56,35,34)
+                assert newPlayer.mPace == 23
+                assert newPlayer.mShooting == 67
+                assert newPlayer.mPassing == 45
+                assert newPlayer.mDribbling == 56
+                assert newPlayer.mDeffending == 35
+                assert newPlayer.mPhysical == 34
+
             
