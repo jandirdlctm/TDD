e17e854155f1a81f59b698c775c8cdeb239c445f
jandirdlctm <d00332071@dmail.dixie.edu>
2021-09-18 20:34:17 -0600

    red

---
+++ test.py
@@ -0,0 +1,6 @@
+import pytest
+
+def describe_SoccerPlayer():
+    def describe_initializer_of_soccer_player():
+        def it_assigns_all_six_attributes():
+                newPlayer = SoccerPlayer(23,67,45,56,35,34)
