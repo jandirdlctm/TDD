6f953a8fbe9862a3d6c35d582b5fb636f951c873
jandirdlctm <d00332071@dmail.dixie.edu>
2021-09-18 20:39:11 -0600

    green

---
+++ player.py
@@ -0,0 +1,8 @@
+class SoccerPlayer:
+    def __init__(self,pace, shooting, passing, dribbling, defending, physical):
+        self.mPace = pace
+        self.mShooting = shooting
+        self.mPassing = passing
+        self.mDribbling = dribbling
+        self.mDeffending = defending
+        self.mPhysical = physical
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import pytest
+from player import SoccerPlayer
 
 def describe_SoccerPlayer():
     def describe_initializer_of_soccer_player():
         def it_assigns_all_six_attributes():
                 newPlayer = SoccerPlayer(23,67,45,56,35,34)
+            
