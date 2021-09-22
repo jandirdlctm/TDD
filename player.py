bd43a112cf99df0fcfbf2ed12ab892c5f4e65421
jandirdlctm <d00332071@dmail.dixie.edu>
2021-09-21 22:06:59 -0600

    green

--- player.py
+++ player.py
@@ -52,14 +52,7 @@ class Player:
         return True 
 
 
-    
-        
-
-
-
-
-
-class SoccerPlayer:
+class SoccerPlayer(Player):
     def __init__(self,pace, shooting, passing, dribbling, defending, physical):
         
         if pace < 0 or pace > 100  or shooting < 0 or shooting > 100 or passing < 0 or passing > 100 or dribbling < 0 or dribbling > 100 or defending < 0 or defending > 100 or physical < 0 or physical > 100:
